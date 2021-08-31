a5dcaf0344e4bdad0aafdd738a398a62ff41387b
Elias Roberts <elias@eroberts.dev>
2021-08-31 13:59:49 -0600

    maybe fixed

--- trader.py
+++ trader.py
@@ -1,6 +1,7 @@
 import config
 import trade_strat
 import win32api
+# from playsound import playsound
 
 import robin_stocks as rh
 import datetime as dt
@@ -69,28 +70,34 @@ if __name__ == "__main__":
         sma = ts.get_sma('ETH', data, window=12)
         p_sma = ts.get_price_sma(ask_price, sma)
         trade = ts.trade_option(p_sma)
+        owned_qty = float(rh.robinhood.crypto.get_crypto_positions(info='quantity_available')[1])
+        balance = get_cash()[0]
         print('Price:', ask_price)
         print('sma: ', sma)
         print('p_sma: ', p_sma)
         print('CHOICE: ', trade)
-        # print('Balance: ', get_cash()[0])
+        print('Balance: ', balance)
         print('Have Bought? ', have_bought)
+        print('Owned: ', owned_qty)
         
         if trade == 'BUY' and have_bought == False:
             print("BOUGHT AT: ", ask_price)
-            rh.robinhood.orders.order_buy_crypto_limit_by_price('ETH', 10000, ask_price, timeInForce='gtc', jsonify=True)
+            rh.robinhood.orders.order_buy_crypto_limit_by_price('ETH', balance - 200, ask_price, timeInForce='gtc', jsonify=True)
             bought_price = ask_price
             have_bought = True
+            # playsound('./ring01.wav')
             win32api.MessageBox(0, 'JUST BOUGHT', 'BUY BUY BUY', 0x00001000)
         elif trade == 'SELL' and have_bought == True:
             print(have_bought)
             print('SOLD AT: ', ask_price)
             sold_price = ask_price
             profit = sold_price - bought_price
+            rh.robinhood.orders.order_sell_crypto_limit_by_price('ETH', owned_qty, ask_price, timeInForce='gtc', jsonify=True)
+            # playsound('./ring01.wav')
             win32api.MessageBox(0, 'SELL SELL SELL', 'SELL SELL', 0x00001000)
             print('PROFIT: ', profit * 3)
             have_bought = False
 
-        time.sleep(15)
+        time.sleep(60)
 
     logout()
